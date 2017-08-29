5474e4e44141f237794fc440e3184ddf8c213265
fredtbx <fredtbx@comcast.net>
2017-08-29 17:39:04 -0400

    2nd commit

---
+++ src/components/AthletePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router';
+import NotFoundPage from './NotFoundPage';
+import AthletesMenu from './AthletesMenu';
+import Medal from './Medal';
+import Flag from './Flag';
+import athletes from '../data/athletes';
+
+export default class AthletePage extends React.Component {
+	const id = this.props.params.id;
+	
+} 
