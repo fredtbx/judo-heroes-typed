1203aed1e6eb9e2e11ceecf8565f98e70003730a
fredtbx <fredtbx@comcast.net>
2017-08-29 18:46:08 -0400

    3rd save

--- src/components/AthletePage.js
+++ src/components/AthletePage.js
@@ -7,6 +7,33 @@ import Flag from './Flag';
 import athletes from '../data/athletes';
 
 export default class AthletePage extends React.Component {
-	const id = this.props.params.id;
+	render() {
+		const id = this.props.params.id;
+		const athlete = athletes.filter((athlete) => athlete.id === id)[0];
+		if (!athlete) {
+			return <NotFoundPage />
+		}
+		const headerStyle = { backgroundImage: `url(/img/${athlete.cover})`};
+		return (
+			<div className="athlete-full">
+				<AthletesMenu athletes={athletes}/>
+				<div className="athlete">
+					<header style={headerStyle}/>
+					<div className="picture-container">
+						<img src={`/img/${athlete.image}`}/>
+						<h2 className="name">{athlete.name}</h2>
+					</div>
+					<section className="description">
+						Olympic medalist from <strong><Flag code={athlete.country} showName="true"/></strong>,
+						born in {athlete.birth} (Find out more on <a href={athlete.link} target="_blank">Wikipedia</a>).
+					</section>
+					<section className="medals">
+					<p>Winner of <strong>{athlete.medals.length}</strong> medals:</p>
+					</div>
+					</div>
+			
+		)
+	}
 	
+
 } 
